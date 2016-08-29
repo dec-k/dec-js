0b7e787fb63e96b19b163936f27bf28141da4474
Dec Keighley <keigdl1@student.op.ac.nz>
2016-08-29 17:30:46 +1200

    mods

--- js/cjs.js
+++ js/cjs.js
@@ -1,20 +1,6 @@
-window.onload = function() {
-  function setup() {
-    createCanvas(640, 480);
-  }
-
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-  }
-};
-
 function setup() {
-  createCanvas(640, 480);
+  var customCanvas = createCanvas(window.innerWidth, window.innerHeight);
+  customCanvas.position(0,0);
 }
 
 function draw() {
