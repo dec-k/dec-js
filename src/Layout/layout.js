711cc54b175cf8285fb215cac0aeb7dbda1d964b
Declan Keighley <dec.keighley@gmail.com>
2019-06-18 21:34:07 +1200

    Scuffed 50/50 layout using only css gradient

--- src/Layout/layout.js
+++ src/Layout/layout.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from '../UI/Header/header'
+import Aux from '../hoc/Auxiliary/auxiliary'
+import classes from './layout.module.css';
 
 /*
 Dictate the layout of constant or near-constant, static page elements like navbars, headers, etc.
 */
 const layout = (props) => {
     return (
-        <Header />
+        <Aux>
+            <div className={classes.Container}>
+                <Header />
+            </div>
+        </Aux>
     );
 }
 
--- src/UI/Header/header.js
+++ src/UI/Header/header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary/auxiliary'
+import classes from './header.module.css'
 
 const header = (props) => {
     return (
         <Aux>
-            <h1>I'm a header</h1>
+            <h1 className={classes.PrimaryHeader}>declan k</h1>
+            <h2 className={classes.SecondaryHeader}>aspiring web dev</h2>
         </Aux>
     );
 }
