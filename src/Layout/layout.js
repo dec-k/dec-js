367fc3b1d0dc5d7f5898fffd79012b624622477c
Dec Keighley <dec.keighley@gmail.com>
2019-06-19 09:52:58 +1200

    implemented sticky navbar

--- src/Layout/layout.js
+++ src/Layout/layout.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import Header from '../UI/Header/header'
 import Aux from '../hoc/Auxiliary/auxiliary'
 import classes from './layout.module.css';
-import Sidebar from '../UI/Sidebar/sidebar';
 import Navbar from '../UI/Navbar/navbar';
 
 /*
-Dictate the layout of constant or near-constant, static page elements like navbars, headers, etc.
+Dictate the layout of constant or near-constant page elements like navbars, headers, etc.
 */
 const layout = (props) => {
     return (
         <Aux>
-            <div className={classes.Container}>
-                <Navbar />
-                
-
-            </div>
+            <Navbar />
+            <div style={{"height": "2000px"}}>fudge</div>
         </Aux>
     );
 }
