fc8bc2a3ca9a89e32091f87425afdc9ca8b7f228
Declan Keighley <dec.keighley@gmail.com>
2019-06-18 21:18:31 +1200

    Imported primary fonts and scuffed layout comps

---
+++ src/Layout/layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../UI/Header/header'
+
+/*
+Dictate the layout of constant or near-constant, static page elements like navbars, headers, etc.
+*/
+const layout = (props) => {
+    return (
+        <Header />
+    );
+}
+
+export default layout;
---
+++ src/UI/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary/auxiliary'
+
+const header = (props) => {
+    return (
+        <Aux>
+            <h1>I'm a header</h1>
+        </Aux>
+    );
+}
+
+export default header;
